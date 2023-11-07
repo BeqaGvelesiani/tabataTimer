ed7890fc5b9cbf370c9c4d9bf364700195a9a5d1
BeqaGvelesiani <107481738+BeqaGvelesiani@users.noreply.github.com>
2023-11-07 19:01:55 +0400

    cx

--- functions.js
+++ functions.js
@@ -1,10 +1,112 @@
 import { data } from "./index.js";
 
 
-export function foo(){
-    setTimeout(log("bingo"), 5000)
+export function speedometer(percent, color) {
+  for (var i = 0; i < percent; i++) {
+    document.getElementById(`speedometerdiv`).innerHTML += `
+          <div class="speedometer" id="speedometer${i}" style="rotate: ${
+      i * 3.6
+    }deg">
+            <div class="line" style="background-color: ${color}" style="box-shadow: ${color}"></div>
+          </div>
+  `;
+  }
 }
 
-function log(a){
-    alert(`${a}`);
+
+export function buttonAction(buttonID, index, buttonVariable) {
+  document.getElementById(`${buttonID}`).addEventListener("click", () => {
+    clrearButtons(index);
+    data[index] = buttonVariable;
+    document.getElementById(`${buttonID}`).className = "btn wider clicked";
+    if (data[3] != 0) {
+      data[4] = 30;
+      document.getElementById("sets").style.display = "block";
+    } else {
+      document.getElementById("sets").style.display = "none";
+      data[4] = 0;
+    }
+    data[index] = buttonVariable;
+    console.log(data);
+    //--------//
+    if (data[0] === 0) {
+      document.getElementById("start").style.boxShadow = "none";
+      document.getElementById("starth1").style.fontWeight = "100";
+    } else {
+      document.getElementById("start").style.boxShadow =
+        "0px 0px 15px 5px green";
+      document.getElementById("starth1").style.fontWeight = "900";
+    }
+    //--------//
+  });
+}
+
+
+
+export function clrearButtons(a) {
+  if (a === 0) {
+    document.getElementById("wt20").className = "btn";
+    document.getElementById("wt30").className = "btn";
+    document.getElementById("wt45").className = "btn";
+    document.getElementById("wt50").className = "btn";
+  }
+  if (a === 1) {
+    document.getElementById("rt10").className = "btn";
+    document.getElementById("rt15").className = "btn";
+    document.getElementById("rt20").className = "btn";
+    document.getElementById("rt30").className = "btn";
+    document.getElementById("rt45").className = "btn";
+  }
+  if (a === 2) {
+    document.getElementById("r4").className = "btn wider";
+    document.getElementById("r6").className = "btn wider";
+    document.getElementById("r8").className = "btn wider";
+    document.getElementById("r10").className = "btn wider";
+    document.getElementById("r12").className = "btn wider";
+  }
+  if (a === 3) {
+    document.getElementById("s1").className = "btn wider";
+    document.getElementById("s2").className = "btn wider";
+    document.getElementById("s3").className = "btn wider";
+    document.getElementById("s4").className = "btn wider";
+    document.getElementById("s5").className = "btn wider";
+  }
+  if (a === 4) {
+    document.getElementById("rr1").className = "btn";
+    document.getElementById("rr2").className = "btn";
+    document.getElementById("rr3").className = "btn";
+    document.getElementById("rr4").className = "btn";
+    document.getElementById("rr5").className = "btn";
+  }
+}
+
+export function setButtons() {
+  buttonAction("wt20", 0, 20);
+  buttonAction("wt30", 0, 30);
+  buttonAction("wt45", 0, 45);
+  buttonAction("wt50", 0, 50);
+  //----------//
+  buttonAction("rt10", 1, 10);
+  buttonAction("rt15", 1, 15);
+  buttonAction("rt20", 1, 20);
+  buttonAction("rt30", 1, 30);
+  buttonAction("rt45", 1, 45);
+  //---------//
+  buttonAction("r4", 2, 4);
+  buttonAction("r6", 2, 6);
+  buttonAction("r8", 2, 8);
+  buttonAction("r10", 2, 10);
+  buttonAction("r12", 2, 12);
+  //---------//
+  buttonAction("s1", 3, 1);
+  buttonAction("s2", 3, 2);
+  buttonAction("s3", 3, 3);
+  buttonAction("s4", 3, 4);
+  buttonAction("s5", 3, 5);
+  //---------//
+  buttonAction("rr1", 4, 30);
+  buttonAction("rr2", 4, 60);
+  buttonAction("rr3", 4, 90);
+  buttonAction("rr4", 4, 120);
+  buttonAction("rr5", 4, 180);
 }
--- index.js
+++ index.js
@@ -1,156 +1,43 @@
 export let data = [0, 0, 0, 0, 0];
 
-import { foo } from "./functions.js";
+import { speedometer } from "./functions.js";
 
-buttonAction("wt20", 0, 5);
-buttonAction("wt30", 0, 30);
-buttonAction("wt45", 0, 45);
-buttonAction("wt50", 0, 50);
-//----------//
-buttonAction("rt10", 1, 10);
-buttonAction("rt15", 1, 15);
-buttonAction("rt20", 1, 20);
-buttonAction("rt30", 1, 30);
-buttonAction("rt45", 1, 45);
-//---------//
-buttonAction("r4", 2, 4);
-buttonAction("r6", 2, 6);
-buttonAction("r8", 2, 8);
-buttonAction("r10", 2, 10);
-buttonAction("r12", 2, 12);
-//---------//
-buttonAction("s1", 3, 1);
-buttonAction("s2", 3, 2);
-buttonAction("s3", 3, 3);
-buttonAction("s4", 3, 4);
-buttonAction("s5", 3, 5);
-//---------//
-buttonAction("rr1", 4, 30);
-buttonAction("rr2", 4, 60);
-buttonAction("rr3", 4, 90);
-buttonAction("rr4", 4, 120);
-buttonAction("rr5", 4, 180);
+import { setButtons } from "./functions.js";
 
-document.getElementById("start").addEventListener("click", function () {
-  if (data[0]) {
-    document.getElementById("input").style.transform = "translateX(-100%)";
-    cycle(data[0], "green", "go");
-    setTimeout(move, data[0] * 1000);
-  }
-});
+setButtons();
 
-function move() {
-  cycle(data[1], "red", "rest");
+// document.getElementById("start").addEventListener("click", function () {
+//   document.getElementById("input").style.transform = "translateX(-100%)";
+//   cycle(data[0], "green", "GO");
+// });
+
+// document.getElementById("back").addEventListener("click", function () {
+//   document.getElementById("input").style.transform = "translateX(0)";
+// });
+
+// function cycle(time, color, message) {
+//   let time_counter = document.getElementById("time_counter");
+//   time_counter.style.color = `${color}`;
+//   time_counter.innerText = `${message}`;
+
+//   let a = time;
+
+//   //setInterval(time, 1000);
+// }
+
+let i = 0
+
+
+setTimeout
+
+//settimo(set, 20/100 * 1000)
+
+
+
+function set(){
+  i++
+  speedometer(i, `green`);
 }
 
 
 
-document.getElementById("back").addEventListener("click", function () {
-  document.getElementById("input").style.transform = "translateX(0)";
-});
-
-function cycle(time, color, message) {
-  //let svg = document.getElementById("SVG");
-  let time_counter = document.getElementById("time_counter");
-  //svg.style.strokeDashoffset = "439.11236572265625";
-  //svg.style.animationDuration = `${time * 1000}ms`;
-  //svg.style.animationName = `anim`;
-  //svg.style.stroke = `${color}`;
-
-  let percent = time/100
-
-  time_counter.style.color = `${color}`;
-  time_counter.innerText = `${message}`;
-
-  let a = time;
-  setInterval(function () {
-    if (a == 0) {
-      clearInterval();
-    } else {
-      a -= 1;
-      time_counter.innerText = a;
-    }
-  }, 1000);
-}
-
-function buttonAction(buttonID, index, buttonVariable) {
-  document.getElementById(`${buttonID}`).addEventListener("click", () => {
-    clrearButtons(index);
-    data[index] = buttonVariable;
-    document.getElementById(`${buttonID}`).className = "btn wider clicked";
-    if (data[3] != 0) {
-      data[4] = 30;
-      document.getElementById("sets").style.display = "block";
-    } else {
-      document.getElementById("sets").style.display = "none";
-      data[4] = 0;
-    }
-    data[index] = buttonVariable;
-    console.log(data);
-    //--------//
-    if (data[0] === 0) {
-      document.getElementById("start").style.boxShadow = "none";
-      document.getElementById("starth1").style.fontWeight = "100";
-    } else {
-      document.getElementById("start").style.boxShadow =
-        "0px 0px 15px 5px green";
-      document.getElementById("starth1").style.fontWeight = "900";
-    }
-    //--------//
-  });
-}
-
-function clrearButtons(a) {
-  if (a === 0) {
-    document.getElementById("wt20").className = "btn";
-    document.getElementById("wt30").className = "btn";
-    document.getElementById("wt45").className = "btn";
-    document.getElementById("wt50").className = "btn";
-  }
-  if (a === 1) {
-    document.getElementById("rt10").className = "btn";
-    document.getElementById("rt15").className = "btn";
-    document.getElementById("rt20").className = "btn";
-    document.getElementById("rt30").className = "btn";
-    document.getElementById("rt45").className = "btn";
-  }
-  if (a === 2) {
-    document.getElementById("r4").className = "btn wider";
-    document.getElementById("r6").className = "btn wider";
-    document.getElementById("r8").className = "btn wider";
-    document.getElementById("r10").className = "btn wider";
-    document.getElementById("r12").className = "btn wider";
-  }
-  if (a === 3) {
-    document.getElementById("s1").className = "btn wider";
-    document.getElementById("s2").className = "btn wider";
-    document.getElementById("s3").className = "btn wider";
-    document.getElementById("s4").className = "btn wider";
-    document.getElementById("s5").className = "btn wider";
-  }
-  if (a === 4) {
-    document.getElementById("rr1").className = "btn";
-    document.getElementById("rr2").className = "btn";
-    document.getElementById("rr3").className = "btn";
-    document.getElementById("rr4").className = "btn";
-    document.getElementById("rr5").className = "btn";
-  }
-}
-
-
-
-
-function speedometer(percent, color) {
-  
-  for (var i = 0; i < percent; i++) {
-    document.getElementById(`speedometerdiv`).innerHTML += `
-          <div class="speedometer" id="speedometer${i}" style="rotate: ${
-      i * 3.6
-    }deg">
-            <div class="line" style="background-color: ${color}"></div>
-          </div>
-  `;
-  }
-}
-
-speedometer(41.2, `red`)
